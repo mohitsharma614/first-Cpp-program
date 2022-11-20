77f59e06fee288e984cdae7e03cf534ccef73df3
Mohit Sharma <mohit2605l@gmail.com>
2022-11-20 08:26:31 +0530

    first commit

---
+++ add_two_no.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two number:";
+    cin>>a>>b;
+   
+    c=a+b;
+    cout<<"sum is:" <<c;
+    return 0;
+}
---
+++ practice.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+  cout<<"May! I know your name:";
+    getline(cin,name);
+  cout<<"welcome Mr."<<name;
+    return 0;
+}
